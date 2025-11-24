15b66987076786981ca660b44f74a171cb06eb5e
AishwaryaPM11 <aishwaryamulagund0@gmail.com>
2025-11-24 16:34:22 +0530

    Create pytestadd2.py

---
+++ pytestadd2.py
@@ -0,0 +1,8 @@
+import sys
+def add(a, b):
+    return a + b
+
+if _name_ == "_main_":
+    x = int(sys.argv[1])   # Convert first argument to integer
+    y = int(sys.argv[2])   # Convert second argument to integer
+    print("sum:", add(x, y))
